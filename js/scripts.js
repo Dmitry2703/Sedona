387fca77c3d4bd33ff57de9ec6d0d3ec541e902a
Dmitry Meshcheryakov <dmitri.meshcheryakov@gmail.com>
2015-04-29 20:04:53 +0600

    Добавил интерактивную карту.

--- js/scripts.js
+++ js/scripts.js
@@ -13,11 +13,16 @@ link.addEventListener("click", function(event) {
 var map;
 function initialize() {
   var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(34.8210272,-111.615511)
+    zoom: 9,
+    center: new google.maps.LatLng(34.7550348,-111.7362262)
   };
   map = new google.maps.Map(document.getElementById("map-canvas"),
       mapOptions);
+  var myLatLng = new google.maps.LatLng(34.8650348,-111.7562262);
+  var beachMarker = new google.maps.Marker( {
+  	position: myLatLng,
+  	map: map,
+  });
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
